6d56d3553a090474f29edb022979cff5ecda15db
Udara <udara28@live.com>
2015-06-15 00:30:26 +0000

    Added timing details to the interface

--- Simulator/sd_intf.py
+++ Simulator/sd_intf.py
@@ -5,6 +5,17 @@ class sd_intf(object):
     addr_width = 12
     data_width = 16
 
+    timing = { # timing details refer data sheet
+        'init' : 100000,    # min init interval
+        'ras'  : 45,        # min interval between active prechargs
+        'rcd'  : 20,        # min interval between active R/W
+        'ref'  : 64000000,  # max refresh interval
+        'rfc'  : 65,        # refresh opertaion
+        'rp'   : 20,        # min precharge
+        'xsr'  : 75,        # exit self-refresh time
+        'wr'   : 55,        # @todo ...
+    }
+
     def __init__(self,clk):
 
         self.clk = clk
