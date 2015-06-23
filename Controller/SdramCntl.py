100deef066f40ff2c1d67f0224b9b52c7feb428e
Udara <udara28@live.com>
2015-06-23 18:18:56 +0000

    Write TeshBench.py to test the controller

--- Controller/SdramCntl.py
+++ Controller/SdramCntl.py
@@ -10,7 +10,7 @@ def SdramCntl(
         addr_i,
         data_i,
         data_o,
-        done_o
+        done_o,
         status_o,
         opBegun_o,
     # SDRAM side
@@ -34,21 +34,22 @@ def SdramCntl(
     PCHG_CMD_C    = 0,0,1,0,0,0
     MODE_CMD_C    = 0,0,0,0,0,0
     RFSH_CMD_C    = 0,0,0,1,0,0
+    
 
     # delay constants
     INIT_CYCLES_C = 4
     RP_CYCLES_C   = 3
     
     # states of the SDRAM controller state machine
-    CntlStateType = (
-            INITWAIT,  # initialization - waiting for power-on initialization to complete.
-            INITPCHG,  # initialization - initial precharge of SDRAM banks.
-            INITSETMODE,                        # initialization - set SDRAM mode.
-            INITRFSH,  # initialization - do initial refreshes.
-            RW,                                 -- read/write/refresh the SDRAM.
-            ACTIVATE,  # open a row of the SDRAM for reading/writing.
-            REFRESHROW,                         -- refresh a row of the SDRAM.
-            SELFREFRESH   # keep SDRAM in self-refresh mode with CKE low.
+    CntlStateType = enum( 
+            'INITWAIT',  # initialization - waiting for power-on initialization to complete.
+            'INITPCHG',  # initialization - initial precharge of SDRAM banks.
+            'INITSETMODE',                        # initialization - set SDRAM mode.
+            'INITRFSH',  # initialization - do initial refreshes.
+            'RW',                                 # read/write/refresh the SDRAM.
+            'ACTIVATE',  # open a row of the SDRAM for reading/writing.
+            'REFRESHROW',                         # refresh a row of the SDRAM.
+            'SELFREFRESH'   # keep SDRAM in self-refresh mode with CKE low.
         );
     
     state_r , state_x = None,None # state register and next state
@@ -59,12 +60,12 @@ def SdramCntl(
     def comb_func():
         
         # Applying commands
-        sdCe_bo, sdRas_bo, sdCas_bo, sdWe_bo = 
+        sdCe_bo, sdRas_bo, sdCas_bo, sdWe_bo, sdDqmh_o, sdDqml_o, = cmd_r
         
         
         if timer_r != 0 :
             timer_x = timer_r - 1
-        else
+        else :
             timer_x = timer_r
             
             if   state_r == CntlStateType.INITWAIT :
@@ -76,16 +77,19 @@ def SdramCntl(
                 # all banks should be precharged after initialization
                 cmd_x                 = PCHG_CMD_C;
                 timer_x               = RP_CYCLES_C;  # set timer for precharge operation duration
-                
+              
+              
+    @always_seq(clk_i.posedge, rst_i)
+    def seq_func():
+        if rst_i == 1 :
+            state_r = CntlStateType.INITWAIT;
+            timer_r = 0
+            cmd_r   = NOP_CMD_C
+            
+        elif clk_i.posedge == 1 :
+            state_r = state_x
+            cmd_r   = cmd_x
+            timer_r = timer_x
+        
 
-    return func
-
-a  = Signal(bool(0))
-b  = Signal(bool(0))
-
-c  = Signal(bool(0))
-
-sdram_Inst = SdramCntl(c,a,b)
-
-sim = Simulation(sdram_Inst)
-sim.run(20)
+    return comb_func, seq_func
