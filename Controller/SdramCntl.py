2e0421f8cb5103c792522953f7631472aea9c53b
Udara <udara28@live.com>
2015-06-26 18:34:09 +0000

    Added new states to the state machine

--- Controller/SdramCntl.py
+++ Controller/SdramCntl.py
@@ -12,10 +12,15 @@ def SdramCntl(host_intf, sd_intf, rst_i):
     MODE_CMD_C    = intbv("000000")[6:]  # 0,0,0,0,0,0
     RFSH_CMD_C    = intbv("000100")[6:]  # 0,0,0,1,0,0
     
+    # mode command for set_mode command
+    MODE_C        = intbv("00_0_00_011_0_000")[12:]
 
     # delay constants
     INIT_CYCLES_C = 10
     RP_CYCLES_C   = 3
+    RFC_CYCLES_C  = 3
+    MODE_CYCLES_C = 3
+    ALL_BANKS_C   = intbv("001000000000")[12:]       # value of CMDBIT to select all banks
     
     # states of the SDRAM controller state machine
     CntlStateType = enum( 
@@ -39,6 +44,12 @@ def SdramCntl(host_intf, sd_intf, rst_i):
     # command assignment
     cmd_r   = Signal(NOP_CMD_C)
     cmd_x   = Signal(NOP_CMD_C)
+    
+    activeRow_r  = Signal(intbv(0)[sd_intf.addr_width:])
+    activeRow_x  = Signal(intbv(0)[sd_intf.addr_width:])
+    activeBank_r = Signal(intbv(0)[4:]) 
+    activeBank_x = Signal(intbv(0)[4:]) # banks with active rows
+    doActivate_s = Signal(bool(0))      # request row activation if the read row is not activated
     
     # pin assignment for SDRAM
     @always_comb
@@ -65,14 +76,34 @@ def SdramCntl(host_intf, sd_intf, rst_i):
             
             if   state_r.val == CntlStateType.INITWAIT :
                 # wait for SDRAM power-on initialization once the clock is stable
-                timer_x.next = INIT_CYCLES_C;  # set timer for initialization duration
-                state_x.next = CntlStateType.INITPCHG;
+                timer_x.next = INIT_CYCLES_C  # set timer for initialization duration
+                state_x.next = CntlStateType.INITPCHG
                 
             elif state_r.val == CntlStateType.INITPCHG :
                 # all banks should be precharged after initialization
-                cmd_x.next                 = PCHG_CMD_C;
-                timer_x.next               = RP_CYCLES_C;  # set timer for precharge operation duration
-              
+                cmd_x.next          = PCHG_CMD_C
+                timer_x.next        = RP_CYCLES_C  # set timer for precharge operation duration
+                state_x.next        = CntlStateType.INITRFSH
+                sd_intf.addr.next   = ALL_BANKS_C  # select all banks precharge   
+            
+            elif state_r.val == CntlStateType.INITRFSH :
+                # refreshing state
+                cmd_x.next          = RFSH_CMD_C
+                timer_x.next        = RFC_CYCLES_C
+                state_x.next        = CntlStateType.INITSETMODE
+                
+            elif state_r.val == CntlStateType.INITSETMODE
+                cmd_x.next          = MODE_CMD_C
+                timer_x.next        = MODE_CYCLES_C
+                state_x.next        = CntlStateType.RW
+                sd_intf.addr.next   = MODE_C
+                
+            elif state_r.val == CntlStateType.RW
+                
+                # for now leave row refresh need.. IT SHOULD COME HERE
+                if host_intf.rd_i.val == True :
+                    
+                
               
     @always_seq(sd_intf.clk.posedge, rst_i)
     def seq_func():
